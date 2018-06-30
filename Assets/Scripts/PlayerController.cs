89c4e43269fada603747386cf559e1a43b3d45f6
Daniel Clarke <danclarke88@gmail.com>
2018-06-30 19:40:58 -0400

    Logic to change lanes, simple demo scene, and logic to keep road infinite.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown("left"))
+            transform.position = transform.position.x <= -1.5 ? transform.position : new Vector3(transform.position.x - 1.5f, transform.position.y);
+        if(Input.GetKeyDown("right"))
+            transform.position = transform.position.x >= 1.5 ? transform.position : new Vector3(transform.position.x + 1.5f, transform.position.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Hazard")
+            Destroy(gameObject);
+    }
 }
---
+++ Assets/Scripts/RoadManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadManager : MonoBehaviour {
+
+    public float speed = 2;
+	
+	void Update () {
+        foreach(Transform child in transform)
+        {
+            child.position = new Vector2(0, child.position.y - Time.deltaTime * speed);
+            if (child.position.y <= -20)
+                GenerateChunk(child);
+        }
+	}
+
+    void GenerateChunk(Transform chunk)
+    {
+        chunk.position = new Vector2(0, chunk.position.y + 60);
+    }
+}
