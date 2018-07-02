02c02e720f36de70b074b17a3bce8c4c92f38ac5
Daniel Clarke <danclarke88@gmail.com>
2018-07-02 13:04:32 -0400

    Logic to more randomly create road hazards

--- Assets/Scripts/RoadManager.cs
+++ Assets/Scripts/RoadManager.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class RoadManager : MonoBehaviour {
 
     public float speed = 2;
-	
+    public GameObject[] hazards;
+
 	void Update () {
         foreach(Transform child in transform)
         {
@@ -18,5 +19,22 @@ public class RoadManager : MonoBehaviour {
     void GenerateChunk(Transform chunk)
     {
         chunk.position = new Vector2(0, chunk.position.y + 60);
+
+        int numHazards = Random.Range(0, 6);
+        foreach (Transform child in chunk.GetChild(3))
+            Destroy(child.gameObject);
+        int spawnY = -8;
+        for (int i = 0; i < numHazards; i++)
+        {
+            int lane = Random.Range(0, 3);
+            Transform newHazard = Instantiate(hazards[Random.Range(0, hazards.Length)], chunk.GetChild(3)).transform;
+            if (lane == 0)
+                newHazard.localPosition = new Vector2(-1.5f, spawnY);
+            else if(lane == 1)
+                newHazard.localPosition = new Vector2(0, spawnY);
+            else
+                newHazard.localPosition = new Vector2(1.5f, spawnY);
+            spawnY += 4;
+        }
     }
 }
